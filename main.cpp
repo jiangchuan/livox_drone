47d81e33d5e7509dbf1e971ce7c0f90f36e336e5
jiangchuan <jiangchuan.huang@gmail.com>
2019-04-07 16:49:19 -0500

    Added Z

--- main.cpp
+++ main.cpp
@@ -13,26 +13,53 @@
 #include <ros/ros.h>
 #include <geometry_msgs/PoseStamped.h>
 #include <geometry_msgs/PoseWithCovarianceStamped.h>
+#include <mavros_msgs/CommandBool.h>
+#include <mavros_msgs/CommandTOL.h>
+#include <mavros_msgs/SetMode.h>
+#include <mavros_msgs/State.h>
+
 #include <tf2/convert.h>
 #include <tf2/LinearMath/Quaternion.h>
 #include <tf2/LinearMath/Transform.h>
 // #include "tf2_geometry_msgs/tf2_geometry_msgs.h"
 
+#include <mavros_msgs/State.h>
 #include <mavros_msgs/Altitude.h>
 #include "sensor_msgs/NavSatFix.h"
+
+#define ROS_RATE 20
+#define UPDATE_JUMP 4 // ROS_RATE / UPDATE_JUMP is the pos update rate
+#define DRONE_SPEED 3.0
+
+#define SCAN_RADIUS 10.0
+#define LAYER_DELTA 5.0
+
+#define DELTA_METERS_V 1.0 // maximum dz
 
 #define PACKET_GAP_MISS_TIME (1500000) // 1.5ms
 #define BD_ARGC_NUM (4)
 #define BD_ARGV_POS (1)
 #define COMMANDLINE_BD_SIZE (15)
 
+mavros_msgs::State current_state;
 geometry_msgs::Pose pose_in;
+geometry_msgs::PoseStamped pose_stamped;
 sensor_msgs::NavSatFix::ConstPtr gps_msg;
 mavros_msgs::Altitude::ConstPtr alt_msg;
 
 double roll = 0.0, pitch = 0.0, yaw = 0.0;
 double worldx = 0.0, worldy = 0.0, worldz = 0.0;
 int year = 0, month = 0, day = 0, hour = 0, minute = 0, second = 0;
+
+double e1x, e1y, e2x, e2y;
+double c1x, c1y, c3x, c3y;
+double c2x, c2y, c4x, c4y;
+int diameter_time;
+
+double destx = 0.0;
+double desty = 0.0;
+double dz = 0.0;
+int travel_time = 0;
 
 std::string timedir;
 std::string gps_filename;
@@ -305,7 +332,7 @@ void GetLidarData(uint8_t handle, LivoxEthPacket *data, uint32_t data_num)
       stream << std::setprecision(4) << pose_in.position.y << ",";
       stream << std::setprecision(4) << pose_in.position.z << ",";
 
-      getRPY(pose_in.orientation);
+      // getRPY(pose_in.orientation);
       compute_world_xyz(tmp_point.z, -tmp_point.y, tmp_point.x);
       stream << std::setprecision(4) << worldx << ",";
       stream << std::setprecision(4) << worldy << ",";
@@ -500,50 +527,92 @@ void OnDeviceBroadcast(const BroadcastDeviceInfo *info)
   }
 }
 
+void state_callback(const mavros_msgs::State::ConstPtr &msg)
+{
+  current_state = *msg;
+}
+
+bool no_position_yet()
+{
+  return fabs(pose_in.orientation.x) + fabs(pose_in.orientation.y) + fabs(pose_in.orientation.z) + fabs(pose_in.orientation.w) < 1e-6;
+}
+
+void delta_position(double dx, double dy, double dz)
+{
+  pose_stamped.pose.position.x += dx;
+  pose_stamped.pose.position.y += dy;
+  pose_stamped.pose.position.z += dz;
+}
+
+int get_travel_params(int curr_loc)
+{
+  if (curr_loc == -1)
+  {
+    dz = DELTA_METERS_V / 2.0;
+    travel_time = diameter_time / 2;
+  }
+  else
+  {
+    dz = DELTA_METERS_V;
+    travel_time = diameter_time;
+  }
+
+  switch (curr_loc)
+  {
+  case -1:
+    curr_loc = 0;
+    destx = e1x;
+    desty = e1y;
+    break;
+
+  case 0:
+    curr_loc = 2;
+    destx = c2x;
+    desty = c2y;
+    break;
+
+  case 1:
+    curr_loc = 2;
+    destx = c2x;
+    desty = c2y;
+    break;
+
+  case 2:
+    curr_loc = 3;
+    destx = c3x;
+    desty = c3y;
+    break;
+
+  case 3:
+    curr_loc = 4;
+    destx = c4x;
+    desty = c4y;
+    break;
+
+  case 4:
+    curr_loc = 1;
+    destx = c1x;
+    desty = c1y;
+    break;
+  }
+  return curr_loc;
+}
+
 int main(int argc, char **argv)
 {
+  // // std::string rootdir = "/home/jiangchuan/livox_data/";
+  // std::string rootdir = "/home/pi/livox_data/";
+  // int status = mkdir(rootdir.c_str(), 0777);
+  // get_time();
+  // std::string time_str = get_time_str();
+  // timedir = rootdir + time_str + "/";
+  // status = mkdir(timedir.c_str(), 0777);
+  // gps_filename = timedir + time_str + ".csv";
 
-  // std::string rootdir = "/home/jiangchuan/livox_data/";
-  std::string rootdir = "/home/pi/livox_data/";
-  int status = mkdir(rootdir.c_str(), 0777);
-
-  get_time();
-  std::string time_str = get_time_str();
-
-  timedir = rootdir + time_str + "/";
-  status = mkdir(timedir.c_str(), 0777);
-
-  gps_filename = timedir + time_str + ".csv";
-
-  if (ros::console::set_logger_level(ROSCONSOLE_DEFAULT_NAME, ros::console::levels::Debug))
-  {
-    ros::console::notifyLoggerLevelsChanged();
-  }
-
-  ROS_INFO("Livox-SDK ros demo");
-
-  if (!Init())
-  {
-    ROS_FATAL("Livox-SDK init fail!");
-    return -1;
-  }
-
-  add_local_broadcast_code();
-  if (argc >= BD_ARGC_NUM)
-  {
-    ROS_INFO("Commandline input %s", argv[BD_ARGV_POS]);
-    add_commandline_broadcast_code(argv[BD_ARGV_POS]);
-  }
-
-  memset(lidars, 0, sizeof(lidars));
-  SetBroadcastCallback(OnDeviceBroadcast);
-  SetDeviceStateUpdateCallback(OnDeviceChange);
-
-  if (!Start())
-  {
-    Uninit();
-    return -1;
-  }
+  // if (ros::console::set_logger_level(ROSCONSOLE_DEFAULT_NAME, ros::console::levels::Debug))
+  // {
+  //   ros::console::notifyLoggerLevelsChanged();
+  // }
 
   /* ros related */
   ros::init(argc, argv, "livox_lidar_publisher");
@@ -553,17 +622,220 @@ int main(int argc, char **argv)
   // Call lidar when in POSITION mode.
   // Make a new folder of timestamp for each flight
 
+  ros::Subscriber state_sub = nh.subscribe<mavros_msgs::State>("mavros/state", 10, state_callback);
   ros::Subscriber local_pos_sub = nh.subscribe<geometry_msgs::PoseStamped>("/mavros/local_position/pose", 10, local_pos_callback);
   ros::Subscriber gps_sub = nh.subscribe<sensor_msgs::NavSatFix>("mavros/global_position/global", 10, gps_callback);
   ros::Subscriber alt_sub = nh.subscribe<mavros_msgs::Altitude>("mavros/altitude", 10, alt_callback);
+  ros::Publisher local_pos_pub = nh.advertise<geometry_msgs::PoseStamped>("mavros/setpoint_position/local", 10);
+  ros::ServiceClient arming_client = nh.serviceClient<mavros_msgs::CommandBool>("mavros/cmd/arming");
+  ros::ServiceClient land_client = nh.serviceClient<mavros_msgs::CommandTOL>("mavros/cmd/land");
+  ros::ServiceClient set_mode_client = nh.serviceClient<mavros_msgs::SetMode>("mavros/set_mode");
 
   ros::Time::init();
-  ros::Rate rate(500); // 500 hz
-  while (ros::ok())
+  //the setpoint publishing rate MUST be faster than 2Hz
+  ros::Rate rate((double)ROS_RATE);
+
+  // wait for FCU connection
+  while (ros::ok() && !current_state.connected)
   {
+    ros::spinOnce();
+    rate.sleep();
+    ROS_INFO("connecting to FCU ...");
+  }
+
+  // // wait for position mode
+  // while (ros::ok() && current_state.mode != "POSCTL")
+  // {
+  //     // std::cout << current_state.mode << std::endl;
+  //     ros::spinOnce();
+  //     rate.sleep();
+  //     ROS_INFO("waiting for position mode ...");
+  // }
+
+  // wait for local position feed
+  while (ros::ok() && no_position_yet())
+  {
+    ros::spinOnce();
+    rate.sleep();
+    ROS_INFO("getting local position ...");
+  }
+
+  // /* Start Livox */
+  // if (!Init())
+  // {
+  //   ROS_FATAL("Livox-SDK init fail!");
+  //   return -1;
+  // }
+
+  // add_local_broadcast_code();
+  // if (argc >= BD_ARGC_NUM)
+  // {
+  //   ROS_INFO("Commandline input %s", argv[BD_ARGV_POS]);
+  //   add_commandline_broadcast_code(argv[BD_ARGV_POS]);
+  // }
+
+  // memset(lidars, 0, sizeof(lidars));
+  // SetBroadcastCallback(OnDeviceBroadcast);
+  // SetDeviceStateUpdateCallback(OnDeviceChange);
+
+  // if (!Start())
+  // {
+  //   Uninit();
+  //   return -1;
+  // }
+  // /* Start Livox Ends*/
+
+  pose_stamped.pose = pose_in;
+
+  //send a few setpoints before starting
+  for (int i = 100; ros::ok() && i > 0; --i)
+  {
+    local_pos_pub.publish(pose_stamped);
     ros::spinOnce();
     rate.sleep();
   }
 
-  Uninit();
+  // change to offboard mode
+  mavros_msgs::SetMode offb_set_mode;
+  offb_set_mode.request.custom_mode = "OFFBOARD";
+  ros::Time last_request = ros::Time::now();
+  while (ros::ok() && current_state.mode != "OFFBOARD")
+  {
+    if (ros::Time::now() - last_request > ros::Duration(5.0))
+    {
+      ROS_INFO(current_state.mode.c_str());
+      if (set_mode_client.call(offb_set_mode) &&
+          offb_set_mode.response.mode_sent)
+      {
+        ROS_INFO("Offboard enabled");
+      }
+      last_request = ros::Time::now();
+    }
+    local_pos_pub.publish(pose_stamped);
+    ros::spinOnce();
+    rate.sleep();
+  }
+
+  // arm
+  mavros_msgs::CommandBool arm_cmd;
+  arm_cmd.request.value = true;
+  while (ros::ok() && !current_state.armed)
+  {
+    if (ros::Time::now() - last_request > ros::Duration(5.0))
+    {
+      if (arming_client.call(arm_cmd) &&
+          arm_cmd.response.success)
+      {
+        ROS_INFO("Vehicle armed");
+      }
+      last_request = ros::Time::now();
+    }
+    local_pos_pub.publish(pose_stamped);
+    ros::spinOnce();
+    rate.sleep();
+  }
+
+  // stay at 0.5m for 5 seconds
+  int n_wait_sec = 2;
+  ROS_INFO("stay at origin for %d seconds", n_wait_sec);
+  delta_position(0.0, 0.0, 1);
+  for (int i = 0; ros::ok() && i < n_wait_sec * ROS_RATE; i++)
+  {
+    local_pos_pub.publish(pose_stamped);
+    ros::spinOnce();
+    rate.sleep();
+  }
+
+  // Mission starts here
+  getRPY(pose_in.orientation); // Get yaw
+  ROS_INFO("  >>> INITIAL yaw = %1.1f degrees", to_degrees(yaw));
+
+  double x0 = pose_stamped.pose.position.x;
+  double y0 = pose_stamped.pose.position.y;
+
+  double radius_sin = SCAN_RADIUS * sin(yaw);
+  double radius_cos = SCAN_RADIUS * cos(yaw);
+  e1x = x0 - radius_sin;
+  e1y = y0 + radius_cos;
+  e2x = x0 + radius_sin;
+  e2y = y0 - radius_cos;
+
+  c1x = e1x + radius_cos;
+  c1y = e1y + radius_sin;
+  c3x = e1x - radius_cos;
+  c3y = e1y - radius_sin;
+
+  c2x = e2x + radius_cos;
+  c2y = e2y + radius_sin;
+  c4x = e2x - radius_cos;
+  c4y = e2y - radius_sin;
+
+  // diameter_time = (int)(std::max(1.0, SCAN_RADIUS / DRONE_SPEED)) * 2; // Constant
+  diameter_time = (int)(M_PI * SCAN_RADIUS / DRONE_SPEED);
+
+  /*  O -> E1 -> {C2 -> C3 -> C4 -> C1} -> {C2 -> C3 -> C4 -> C1} */
+  /* -1 ->  0 -> { 2 ->  3 ->  4 ->  1} -> { 2 ->  3 ->  4 ->  1} */
+
+  int curr_loc = -1;
+
+  for (int i = 0; i < 20; i++)
+  {
+    std::cout << curr_loc << " -> ";
+    curr_loc = get_travel_params(curr_loc);
+    std::cout << curr_loc << std::endl;
+
+    // travel2next() starts
+    double dxr = (destx - pose_in.position.x) / 2.0;
+    double dyr = (desty - pose_in.position.y) / 2.0;
+    int num_updates = travel_time * ROS_RATE / UPDATE_JUMP;
+    double dtheta = M_PI / (double)num_updates;
+    double idz = dz / (double)num_updates;
+    for (int i = 0; i < num_updates; i++)
+    {
+      double cos_coef = cos(dtheta * i) - cos(dtheta * (i + 1));
+      double idx = dxr * cos_coef;
+      double idy = dyr * cos_coef;
+      delta_position(idx, idy, idz);
+      for (int j = 0; ros::ok() && j < UPDATE_JUMP; j++)
+      {
+        local_pos_pub.publish(pose_stamped);
+        ros::spinOnce();
+        rate.sleep();
+      }
+    }
+    // travel2next() ends
+  }
+
+  mavros_msgs::CommandTOL land_cmd;
+  land_cmd.request.yaw = 0.0f;
+  land_cmd.request.latitude = 0.0f;
+  land_cmd.request.longitude = 0.0f;
+  land_cmd.request.altitude = 0.0f;
+
+  ROS_INFO("tring to land");
+  while (ros::ok())
+  {
+    if (ros::Time::now() - last_request > ros::Duration(5.0))
+    {
+      if (land_client.call(land_cmd) && land_cmd.response.success)
+      {
+        ROS_INFO("Landing");
+        break;
+      }
+      last_request = ros::Time::now();
+    }
+    local_pos_pub.publish(pose_stamped);
+    ros::spinOnce();
+    rate.sleep();
+  }
+
+  //Manual: MANUAL
+  //Return: AUTO.RTL
+  //AUTO_MISSION
+  //Hold: AUTO.LOITER
+  //Stabilized: STABILIZED
+  //Altitude: ALTCTL
+  //Follow Me: AUTO.FOLLOW_TARGET
+
+  // Uninit();
 }
