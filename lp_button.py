379fec41b813cab9e06e99ac3724d5fc39a9b96a
jiangchuan <jiangchuan.huang@gmail.com>
2019-04-21 11:26:44 -0500

    Rewrite lp_button.py

--- lp_button.py
+++ lp_button.py
@@ -3,52 +3,44 @@ import RPi.GPIO as GPIO
 import time
 import subprocess
 
-# we will use the pin numbering to match the pins on the Pi, instead of the
-# GPIO pin outs (makes it easier to keep track of things)
 
-pinNum = 5
+pinNum = 5  # use the same pin that is used for the reset button, one button to rule them all!
+
 GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)
-# use the same pin that is used for the reset button (one button to rule them all!)
 GPIO.setup(pinNum, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
+maxCycle = 100
+shortThresh = 10
+longThresh = 30
 while True:
     if GPIO.input(pinNum) == 0:
-
         i = 0
-        for i in range(100):
-            if GPIO.input(pinNum): # not pressed any more
-                print('test gpio input')
-                longPress = False
+        for i in range(maxCycle):
+            if GPIO.input(pinNum):  # not pressed any more
                 break
-     	    time.sleep(0.1)
-    
-        if i < 10:
-            print('short press starts')
-	    #TODO
-	    time.sleep(5)
-	    print('short ends')
-        elif i >= 30:
-            print('long press starts')
-	    #TODO
-	    time.sleep(5)
-	    print('long ends')
-	else:
-	   print('middle do nothing')
+            time.sleep(0.1)
+
+        if i < shortThresh:  # short press
+            subprocess.call('/bin/bash -c "source /opt/ros/kinetic/setup.bash; source /home/pi/catkin_ws/devel/setup.bash; /usr/bin/python /opt/ros/kinetic/bin/roslaunch livox_drone livox_drone.launch"', shell=True)
+        elif i >= longThresh:
+            subprocess.call('halt', shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+        time.sleep(5)  # sleep some time so that it does not go to next iteration immediately
 
     time.sleep(0.1)
 
 
 #oldState = True
-#while True:
+# while True:
 #    newState = GPIO.input(pinNum)
 #    # check to see if button has been pushed
 #    if newState != oldState and newState == False:
 #        print('the single press')
 #        oldState = newState
-#    time.sleep(.1)    
+#    time.sleep(.1)
 
-#while True:
+# while True:
 #    # set an interrupt on a falling edge and wait for it to happen
 #    GPIO.wait_for_edge(pinNum, GPIO.FALLING)
 #    # we got here because the button was pressed.
@@ -70,15 +62,13 @@ while True:
 #        longPress = False
 #        break
 #      time.sleep(0.1)
-    
+
 #    if longPress:
 #      print('long press\n')
 #    else:
 #      print('single press\n')
 
 #    time.sleep(0.5)
-
-
 
 
 #     time.sleep(3)
