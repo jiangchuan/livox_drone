cb736e3cc0e9ab87cfceff4bba42ae870c33d0f0
jiangchuan <jiangchuan.huang@gmail.com>
2019-04-21 11:12:18 -0500

    good lp_button.py

--- lp_button.py
+++ lp_button.py
@@ -13,24 +13,70 @@ GPIO.setwarnings(False)
 GPIO.setup(pinNum, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 while True:
-    # set an interrupt on a falling edge and wait for it to happen
-    GPIO.wait_for_edge(pinNum, GPIO.FALLING)
-    # we got here because the button was pressed.
-    # wait for 3 seconds to see if this was deliberate
+    if GPIO.input(pinNum) == 0:
 
-    longPress = True
-    for i in range(30):
-      if GPIO.input(pinNum): # not pressed any more
-        longPress = False
-        break
-      time.sleep(0.1)
+        i = 0
+        for i in range(100):
+            if GPIO.input(pinNum): # not pressed any more
+                print('test gpio input')
+                longPress = False
+                break
+     	    time.sleep(0.1)
     
-    if longPress:
-      print('long press')
-    else:
-      print('single press')
+        if i < 10:
+            print('short press starts')
+	    #TODO
+	    time.sleep(5)
+	    print('short ends')
+        elif i >= 30:
+            print('long press starts')
+	    #TODO
+	    time.sleep(5)
+	    print('long ends')
+	else:
+	   print('middle do nothing')
 
     time.sleep(0.1)
+
+
+#oldState = True
+#while True:
+#    newState = GPIO.input(pinNum)
+#    # check to see if button has been pushed
+#    if newState != oldState and newState == False:
+#        print('the single press')
+#        oldState = newState
+#    time.sleep(.1)    
+
+#while True:
+#    # set an interrupt on a falling edge and wait for it to happen
+#    GPIO.wait_for_edge(pinNum, GPIO.FALLING)
+#    # we got here because the button was pressed.
+#    # wait for 3 seconds to see if this was deliberate
+
+#    time.sleep(3)
+#    # check the button level again
+#    if GPIO.input(pinNum):
+#        # still pressed, it must be a serious request; shutdown Pi
+#        print('the short press')
+#    else:
+#	print('the long press')
+
+
+#    longPress = True
+#    for i in range(30):
+#      if GPIO.input(pinNum): # not pressed any more
+#	print('test gpio input')
+#        longPress = False
+#        break
+#      time.sleep(0.1)
+    
+#    if longPress:
+#      print('long press\n')
+#    else:
+#      print('single press\n')
+
+#    time.sleep(0.5)
 
 
 
